923e435e10acb4af70d94a27745fad9812cc13c5
tkdlek11112 <tkdlek11112@gmail.com>
2021-11-27 23:01:24 +0900

    2021.11.28-수업

--- content/views.py
+++ content/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from uuid import uuid4
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import os
 
-# Create your views here.
+from content.models import Feed
+from learningspoons.settings import MEDIA_ROOT
+
+
+class Test(APIView):
+    def get(self, request):
+        print("GET 요청이 들어옴")
+        return Response(status=200, data=dict(message="GET 호출"))
+
+    def post(self, request):
+        print("POST 요청이 들어옴")
+
+        file = request.FILES['file']    # 인풋에서 파일 가져오기
+        content = request.data.get('content')   # 인풋에서 글 내용 가져오기
+        profile_image = request.data.get('profile_image')   # 인풋에서 프로필 이미지 가져오기
+        nickname = request.data.get('nickname')     # 인풋에서 닉네임 가져오기
+        print(file, content, profile_image, nickname)   # 출력
+
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        Feed.objects.create(content=content, image=uuid_name, profile_image=profile_image, nickname=nickname)
+
+        return Response(status=200, data=dict(message="성공"))
--- learningspoons/urls.py
+++ learningspoons/urls.py
@@ -13,12 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from content.views import Test
+from learningspoons import settings
 from learningspoons.views import Main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', Main.as_view()),
+    path('content/test', Test.as_view())
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
