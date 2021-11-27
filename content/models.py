6bca36a3e2fe15c4ba4476b12cd4d182c3b447ab
jin.99 <mychew@kakao.com>
2021-11-27 15:23:35 +0900

    2021.11.24

--- content/models.py
+++ content/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Feed(models.Model):
+    content = models.TextField()
+    image = models.TextField()
+    profile_image = models.TextField()
+    nickname = models.TextField()
--- learningspoons/views.py
+++ learningspoons/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 
+from content.models import Feed
+
 
 class Main(APIView):
     def get(self, request):
+
         return render(request, 'learningspoons/main.html')
