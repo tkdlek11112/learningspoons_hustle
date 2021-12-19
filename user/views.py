fe2d0aa5cc80462d7c14f1532faa4785c670c516
jin.99 <mychew@kakao.com>
2021-12-19 11:20:09 +0900

    ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹ

--- user/views.py
+++ user/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from user.models import User
 
-
+# 테스트
 class Join(APIView):
     def get(self, request):
         return render(request, 'user/join.html')
